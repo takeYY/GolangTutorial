be61894fa7c133fe401ac9fb971ac297600b1e58
takeYY <t0k6yist@gmail.com>
2024-06-26 22:48:53 +0900

    add: endpoint of user JSON

--- server.go
+++ server.go
@@ -11,5 +11,21 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/users/:name", getUserName)
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+// User
+type User struct {
+  Name  string `json:"name" xml:"name"`
+  Description string `json:"description" xml:"description"`
+}
+
+func getUserName(c echo.Context) error {
+	name := &User{
+		Name: c.Param("name"),
+		Description: "説明のテストですよ",
+	}
+
+	return c.JSON(http.StatusOK, name)
 }
