90fc3b02af69da4991d2ff4750e7fadb11ece172
takeYY <t0k6yist@gmail.com>
2024-07-06 17:19:20 +0900

    tests: add a test for hello world

---
+++ src/apps/hello/hello_test.go
@@ -0,0 +1,23 @@
+package hello
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHello(t *testing.T) {
+	e := echo.New()
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/?pretty", nil)
+	c := e.NewContext(req, rec)
+
+	err := c.String(http.StatusOK, "Hello, World!")
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Hello, World!", rec.Body.String())
+	}
+}
