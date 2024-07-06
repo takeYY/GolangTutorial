2428cbdc169d6c63d665f44a28e4adf0897c29c8
takeYY <t0k6yist@gmail.com>
2024-07-06 18:04:31 +0900

    tests: add a user test to get

---
+++ src/apps/users/user_test.go
@@ -0,0 +1,25 @@
+package users
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+
+
+func TestGetUserName(t *testing.T) {
+	e := echo.New()
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/users/", nil)
+	c := e.NewContext(req, rec)
+
+	err := c.JSON(http.StatusOK, User{Name: "John Doe", Description: "説明のテストですよ"})
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.JSONEq(t, `{"name":"John Doe", "description":"説明のテストですよ"}`, rec.Body.String())
+	}
+}
