8c0969030a66805be323d2a15b11bae8d764c8a9
takeYY <t0k6yist@gmail.com>
2024-07-17 08:24:30 +0900

    add: models and query that created by gorm gen

--- src/model/movie_model.go
+++ src/model/movie_model.go
@@ -1,14 +1,1 @@
 package model
-
-import (
-	"database/sql"
-	"time"
-)
-
-type Movie struct {
-	ID 				int 						`gorm:"primary_key;column:id"`
-	CreatedAt time.Time 			`gorm:"column:created_at"`
-	UpdatedAt time.Time 			`gorm:"column:updated_at"`
-	Title 		string 					`gorm:"column:title"`
-	Overview 	sql.NullString 	`gorm:"column:overview"`
-}
