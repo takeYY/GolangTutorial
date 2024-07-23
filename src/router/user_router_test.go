8ed0b69f19f364fc1f40b4a469f4f3b536bf9b40
takeYY <t0k6yist@gmail.com>
2024-07-24 06:21:12 +0900

    tests: fix user test

--- src/router/user_router_test.go
+++ src/router/user_router_test.go
@@ -7,22 +7,56 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
-
-	"golang_tutorial/src/model"
 )
 
-
-
 func TestGetUserName(t *testing.T) {
-	e := echo.New()
-	rec := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodGet, "/users/", nil)
-	c := e.NewContext(req, rec)
-
-	// NOTE: 関数のテストではなく、サーバを立ててテストした方が良いかも
-	err := c.JSON(http.StatusOK, model.User{Name: "John Doe", Description: "説明のテストですよ"})
-	if assert.NoError(t, err) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.JSONEq(t, `{"name":"John Doe", "description":"説明のテストですよ"}`, rec.Body.String())
+	tests := []struct {
+		name     string
+		expected string
+	}{
+		{name: "John Doe", expected: `{"name":"John Doe", "description":"テストの説明_1"}`},
+		{name: "Jane Doe", expected: `{"name":"Jane Doe", "description":"説明_2"}`},
 	}
+
+	for _, tt := range tests {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/users", nil)
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+
+		c := e.NewContext(req, rec)
+		c.SetPath("/users/:name")
+		c.SetParamNames("name")
+		c.SetParamValues(tt.name)
+
+		err := GetUserName(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.JSONEq(t, tt.expected, rec.Body.String())
+		}
+	}
+}
+
+func TestGetUserNameNotFound(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+	req.Header.Set("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+	c.SetPath("/users/:name")
+	c.SetParamNames("name")
+	c.SetParamValues("invalid user")
+
+	err := GetUserName(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+	assert.JSONEq(t, `{"message":"invalid user is not found"}`, rec.Body.String())
 }
