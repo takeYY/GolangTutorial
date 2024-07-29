e02966c3e24a3622918b3670df59d5d4cc88d5d3
takeYY <t0k6yist@gmail.com>
2024-07-30 08:39:14 +0900

    refactor: apply code formatter

--- scripts/generate_models.go
+++ scripts/generate_models.go
@@ -6,17 +6,16 @@ import (
 	"gorm.io/gen"
 )
 
-
 func main() {
 	g := gen.NewGenerator(gen.Config{
-    OutPath: "../src/query",
-    Mode: gen.WithoutContext,
-  })
+		OutPath: "../src/query",
+		Mode:    gen.WithoutContext,
+	})
 
 	gormdb := repository.ConnectDB()
 
-  // gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
-  g.UseDB(gormdb) // reuse your gorm db
+	// gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(gormdb) // reuse your gorm db
 
 	all := g.GenerateAllTable() // database to table model
 
