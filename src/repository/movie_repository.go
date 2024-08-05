60033fc0d5aef96565653feaaa9b3758d85ab415
takeYY <t0k6yist@gmail.com>
2024-08-06 02:16:27 +0900

    update: repository in movie

--- src/repository/movie_repository.go
+++ src/repository/movie_repository.go
@@ -11,44 +11,24 @@ type MovieRepository struct {
 	Session *gorm.DB
 }
 
-func (r *MovieRepository) GetMovieById(id *int32) (*model.MovieWithGenres, error) {
+func (r *MovieRepository) GetMovieById(id *int32) (*model.Movie, error) {
 	m := query.Use(r.Session).Movie
-	g := query.Use(r.Session).Genre
-	mg := query.Use(r.Session).MovieGenre
 
-	type Result struct {
-		Title    string
-		Overview string
-		Name     string
-	}
-	var results []Result
-
-	err := mg.Where(mg.MovieID.Eq(*id)).Select(m.Title, m.Overview, g.Name).LeftJoin(m, m.ID.EqCol(mg.MovieID)).LeftJoin(g, g.ID.EqCol(mg.GenreID)).Scan(&results)
-	if err != nil {
-		return nil, err
+	movie, e := m.Preload(m.Genres).Where(m.ID.Eq(*id)).First()
+	if e != nil {
+		return nil, e
 	}
 
-	var genres []string
-	for _, result := range results {
-		genres = append(genres, result.Name)
-	}
-
-	movieWithGenre := model.MovieWithGenres{
-		Title:    results[0].Title,
-		Overview: results[0].Overview,
-		Genres:   genres,
-	}
-
-	return &movieWithGenre, nil
+	return movie, nil
 }
 
-func (r *MovieRepository) GetMovies() ([]model.Movie, error) {
-	var movies []model.Movie
+func (r *MovieRepository) GetMovies() ([]*model.Movie, error) {
+	m := query.Use(r.Session).Movie
 
-	err := r.Session.Preload("Genres").Find(&movies).Error
+	movie, err := m.Preload(m.Genres).Find()
 	if err != nil {
 		return nil, err
 	}
 
-	return movies, nil
+	return movie, nil
 }
