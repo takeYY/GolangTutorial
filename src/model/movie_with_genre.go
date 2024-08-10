a247dc3761354a7d87f8ff1fbc2c5aa3dd443911
takeYY <t0k6yist@gmail.com>
2024-08-10 15:47:04 +0900

    delete: src and server.go

--- src/model/movie_with_genre.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type MovieWithGenres struct {
-	Title    string   `json:"title"`
-	Overview string   `json:"overview"`
-	Genres   []string `json:"genres"`
-}
--- src/model/user_model.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type User struct {
-	Name        string `json:"name" xml:"name"`
-	Description string `json:"description" xml:"description"`
-}
